df4f68da43932a505159b55802b2f7adb7ae806f
kurniadikevin <kurniadikevin@gmail.com>
2022-08-25 20:53:47 +0700

    add redirect and username on dashboard

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Dashboard =() => {
 
+    const [ profileName,setProfileName] = useState('')
+
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        if(user.displayName){
+         setProfileName('@' + user.displayName);
+        } else{
+            setProfileName('Anon');
+        }
+      } else {
+        // User is signed out
+       setProfileName('Guest');
+
+      }
+    });
 
     return(
         <div className='dashboard'>
@@ -9,13 +28,23 @@ const Dashboard =() => {
                 <div >Twittish</div>
             </div>
             <nav className='nav-link'>
-                <Link to = '/' className='class-link'>Home</Link>
-                <Link to = '/profile' className='class-link'>Profile</Link>
-                <Link to = '/message' className='class-link'>Message</Link>
+                <Link to = '/' className='class-link'>
+                    <span class="material-symbols-outlined">
+                    other_houses</span>
+                     Home</Link>
+                <Link to = '/profile' className='class-link'>
+                    <span class="material-symbols-outlined">
+                    person_4</span>
+                    Profile</Link>
+                <Link to = '/message' className='class-link'>
+                <span class="material-symbols-outlined">
+                    inbox</span>
+                    Message</Link>
                 <Link to = '/signPage' className='class-link'>Sign-Page</Link> 
             </nav>
             <div className='profile-icon'>
-                Profile Icon
+                <div className='profile-pic'>PP</div>
+                <div className='profile-name'>{profileName}</div>
             </div>
         </div>
     )
--- src/sign-page.js
+++ src/sign-page.js
@@ -1,6 +1,63 @@
 import './sign-page.css';
+import { getAuth, createUserWithEmailAndPassword,  signInWithEmailAndPassword  } from "firebase/auth";
+import { app } from './firebase.js';
+import {useNavigate} from 'react-router-dom';
+import { useCallback, useState } from 'react';
 
 const SignPage = ()=> {
+
+   
+
+ //sign up function   
+ const signUpNewUser = () => {
+    let email = document.querySelector('#user-email').value;
+    let password = document.querySelector('#user-password').value;
+    let userName = document.querySelector('#user-username').value;
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        user.displayName = userName;
+        console.log(user);
+        redirectFunc();
+        alert('account created');     
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode + errorMessage);
+      });
+ }
+    
+    //log in function
+    const logInUser = () => {
+        let email = document.querySelector('#user-email').value;
+        let password = document.querySelector('#user-password').value;
+        let userName = document.querySelector('#user-username').value;
+
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            user.displayName = userName;
+            console.log(user);
+            redirectFunc();
+            alert('sign in succesfully');        
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorCode + errorMessage);//
+        });
+    }
+
+    //redirect function
+    const navigate = useNavigate();
+    const redirectFunc = useCallback(() => navigate('/', {replace: true}), [navigate]);
+    
 
     return(
         <div className="sign-page">
@@ -9,19 +66,29 @@ const SignPage = ()=> {
                     <div className="sign-title">
                         <div>Twittish</div>
                     </div>
+                    <div className='username-cont'>
+                        <label>Username</label>
+                        <input type={'text'} id='user-username'></input>
+                    </div>
                     <div className='email-cont'>
                         <label>Email</label>
-                        <input type={'email'}></input>
+                        <input type={'email'} id='user-email'></input>
                     </div>
                     <div className='password-cont'>
                         <label>Password</label>
-                        <input type={'password'}></input>
+                        <input type={'password'} id='user-password'></input>
                     </div>
 
                     <div className='button-cont'>
                         <div className='button-wrapper'>
-                        <button className='signUp-btn'>Sign Up</button>
-                        <button className='logIn-btn'>Log in</button>
+                        
+                            <button className='signUp-btn' onClick={signUpNewUser}>
+                             Sign Up</button>
+                        
+                        
+                            <button className='logIn-btn' onClick={logInUser}
+                            >Log in</button>
+                        
                         </div>
                     </div>
             
